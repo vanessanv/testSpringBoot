6498befa17946e437e1fdcc763bcdfc9d4f6b132
vanessanv <nessa.vnv@gmail.com>
2018-08-26 22:11:15 -0300

    primeira versao

---
+++ src/main/java/br/com/mutant/MutantApplication.java
@@ -0,0 +1,21 @@
+package br.com.mutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MutantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello Spring Boot!";
+	}
+
+}
